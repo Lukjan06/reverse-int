32e41bf10a3723268ed267f866835c96d84431e0
Anastasya <elka-mogilev@mail.ru>
2022-01-27 14:16:39 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+let num;
+if ( n < 0 ) {
+    num = Math.abs(n)
+} else { 
+    num = n;
+}
+let str = num.toString();
+let newStr = '';
+for (let i = str.length-1; i >= 0; i--)
+{
+    newStr = newStr + str[i];
+}
+return Number(newStr);
 }
